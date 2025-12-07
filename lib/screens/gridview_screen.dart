c86842a28e996337377aec27ec1424495eaf8502
GAURAB <gaurabkhadka4082@gmail.com>
2025-12-07 12:39:02 +0545

    classwork

--- lib/screens/gridview_screen.dart
+++ lib/screens/gridview_screen.dart
@@ -10,8 +10,25 @@ class GridViewScreen extends StatelessWidget {
         title: const Text('GridView'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text('gridview', style: TextStyle(fontSize: 24)),
+      body: GridView.count(
+        padding: EdgeInsets.all(8),
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        crossAxisCount: 3,
+        children: [
+          for (int i = 1; i <= 10; i++) ...{
+            SizedBox(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.grey,
+                ),
+                child: Text("$i", style: TextStyle(fontSize: 50)),
+              ),
+            ),
+          },
+        ],
       ),
     );
   }
