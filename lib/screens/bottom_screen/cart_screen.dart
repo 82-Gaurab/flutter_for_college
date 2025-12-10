78128f7af1639b4d2dca52e6bec785b16e4d1854
GAURAB <gaurabkhadka4082@gmail.com>
2025-12-10 20:02:43 +0545

    Add Simple interest in cart and addition in home

--- lib/screens/bottom_screen/cart_screen.dart
+++ lib/screens/bottom_screen/cart_screen.dart
@@ -1,10 +1,96 @@
 import 'package:flutter/material.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  final TextEditingController _principleController = TextEditingController();
+  final TextEditingController _rateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+
+  String result = "";
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(child: Center(child: Text("Cart")));
+    return SizedBox.expand(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Enter Principle",
+                  hintText: "10000",
+                ),
+                controller: _principleController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter a value";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Enter Rate",
+                  hintText: "5",
+                ),
+                controller: _rateController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter a value";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Enter Time",
+                  hintText: "10",
+                ),
+                controller: _timeController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter a value";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() {
+                      double si =
+                          (double.parse(_principleController.text) *
+                              double.parse(_rateController.text)) *
+                          double.parse(_timeController.text) /
+                          100;
+                      result = "$si";
+                    });
+                  }
+                },
+                child: Text('Calculate'),
+              ),
+
+              SizedBox(height: 30),
+              Text(result, style: TextStyle(fontSize: 30)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/bottom_screen/home_screen.dart
+++ lib/screens/bottom_screen/home_screen.dart
@@ -8,58 +8,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String result = '';
+
+  final TextEditingController _firstNoController = TextEditingController();
+  final TextEditingController _secondNoController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    String result = '';
-
-    final TextEditingController _firstNoController = TextEditingController();
-    final TextEditingController _secondNoController = TextEditingController();
-
     return SizedBox.expand(
       child: Form(
         key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "12",
-                labelText: "First Number",
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "12",
+                  labelText: "First Number",
+                ),
+                keyboardType: TextInputType.number,
+                controller: _firstNoController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter a value";
+                  }
+                  return null;
+                },
               ),
-              keyboardType: TextInputType.number,
-              controller: _firstNoController,
-            ),
 
-            SizedBox(height: 30),
+              SizedBox(height: 30),
 
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "10",
-                labelText: "Second Number",
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "10",
+                  labelText: "Second Number",
+                ),
+                controller: _secondNoController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter a value";
+                  }
+                  return null;
+                },
               ),
-              controller: _secondNoController,
-              keyboardType: TextInputType.number,
-            ),
 
-            SizedBox(height: 20),
+              SizedBox(height: 20),
 
-            ElevatedButton(
-              child: Text("Add"),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  setState(() {
-                    int sum =
-                        int.parse(_firstNoController.text) +
-                        int.parse(_secondNoController.text);
-                    result = "$sum";
-                  });
-                }
-              },
-            ),
-            SizedBox(height: 20),
+              ElevatedButton(
+                child: Text("Add"),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() {
+                      int sum =
+                          int.parse(_firstNoController.text) +
+                          int.parse(_secondNoController.text);
+                      result = "$sum";
+                    });
+                  }
+                },
+              ),
+              SizedBox(height: 20),
 
-            Text(result, style: TextStyle(fontSize: 30)),
-          ],
+              Text(result, style: TextStyle(fontSize: 30)),
+            ],
+          ),
         ),
       ),
     );
